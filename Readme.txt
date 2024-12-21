e1f2e1ab19b0008f0714563025a5b8ba7e40b2a8
Lori Brown <34607662+Lori2018@users.noreply.github.com>
2024-12-21 12:10:55 -0500

    moved files out of dist

---
+++ Readme.txt
@@ -0,0 +1,2 @@
+To run locally: 
+npx vite
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "../node_modules/three/build/three.module.js";
+import * as THREE from "three";
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
 const scene = new THREE.Scene();
